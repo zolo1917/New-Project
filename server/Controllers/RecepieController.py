df902deafc8a2828f2fdcc529c185b9dfe144c66
swatibajaj54 <swatibajaj54@gmail.com>
2022-09-19 23:37:12 +0530

    code restructure

--- server/Controllers/RecepieController.py
+++ server/Controllers/RecepieController.py
@@ -5,17 +5,17 @@ from logging import getLogger
 
 
 logger = getLogger(__name__)
-router = APIRouter(prefix="/user")
+router = APIRouter(prefix="/recepie")
 
 recepie = {
-   1:{
+   "1":{
       "user_id": 11,
       "recipe_name":"pao_bhaji",
       "recipe_description":"punjabi new style pao bhaji",
       "created_date":"2020",
       "last_modified_date":'2021'
    },
-   2:{
+   "2":{
       "user_id":12,
       "recipe_name":"chhole bhatura",
       "recipe_description":"punjabi chhole bhature",
@@ -27,10 +27,12 @@ recepie = {
 @router.get("/getRecepieById/{recepie_id}")
 async def get_recepie_by_id(recepie_id):
     logger.info(f"fetching recepie for {recepie_id}")
-    for recepie_id in recepie:     
-        if recepie[recepie_id] == recepie_id:
-            return recepie[recepie_id]
-    return {"Data": "Not found"}
+    print('Dict: ', recepie)
+   
+    if recepie_id in recepie:     
+         return recepie[recepie_id]
+    else:
+      return {"Data": "Not found"}
 
 
 @router.get("/getAllRecepie")
