ad3e41e9fcc28f512ed93160fe69cf87e637047c
sagarvns <90026062+sagarvns@users.noreply.github.com>
2022-09-23 22:39:21 +0530

    Update Optional

--- server/Models/CommentModel.py
+++ server/Models/CommentModel.py
@@ -1,8 +1,10 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class CommentModel(BaseModel):
-    id:int
+    id:Optional[int]
     recipe_id: int
     user_id: int
     details: str
