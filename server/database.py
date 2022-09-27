8f98eb7c07ce789349c06a519bcb9248a94b1651
zolo1917 <adwaith_venkatesh@msn.com>
2022-09-27 21:09:22 +0530

    update the database configurations

---
+++ server/database.py
@@ -0,0 +1,30 @@
+
+from os import environ as env
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+load_dotenv()
+
+user = env.get("local.db.user")
+password = env.get("local.db.password")
+host = env.get("local.db.host")
+port = env.get("local.db.port")
+database = env.get("local.db.database")
+
+SQL_SESSION_URL = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
+
+engine = create_engine(SQL_SESSION_URL, echo=True)
+
+SessionLocal = sessionmaker(autoflush=False, autocommit=True, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
